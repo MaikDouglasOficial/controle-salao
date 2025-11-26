2ebc4933a99f6811715e329534c57446949d2835
MaikDouglasOficial <maik.ferreira@estudante.ifto.edu.br>
2025-11-25 22:05:55 -0300

    Feat: Migração completa para PostgreSQL - Remove SQLite e cria schema inicial

--- prisma/migrations/20251124031108_add_photo_fields/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Customer" ADD COLUMN "photo" TEXT;
-
--- AlterTable
-ALTER TABLE "Product" ADD COLUMN "photo" TEXT;
-
--- AlterTable
-ALTER TABLE "Professional" ADD COLUMN "photo" TEXT;
