506ad2c1d4849fb617ee3d78a4567ca5e5b0bdeb
MaikDouglasOficial <maik.ferreira@estudante.ifto.edu.br>
2025-11-26 21:44:58 -0300

    Fix: Desativada toolbar de build do Next.js/Vercel

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  devIndicators: {
+    buildActivity: false,
+    buildActivityPosition: 'bottom-right',
+  },
 }
 
 module.exports = nextConfig
